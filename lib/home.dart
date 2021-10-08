53f54f4dc720015255d8e28f9e50f2fa53a5c55d
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2021-10-08 11:32:31 +0530

    resolved: blank screen on SignIn

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:scan_it/dashboard.dart';
 import 'package:scan_it/signin.dart';
 
 
@@ -24,7 +25,7 @@ class _HomeState extends State<Home> {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context,snapshot){
         if(_auth.currentUser!=null){
-          return Container();
+          return Dashboard();
         }
         else{
           return SignIn();
---
+++ lib/scanner.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Scanner extends StatefulWidget {
+  const Scanner({ Key? key }) : super(key: key);
+
+  @override
+  _ScannerState createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
