6301641c5a757fb8dcc72ff6a37dcd5e3f4b8a6f
koushik310301 <koushikeduacc@gmail.com>
2021-10-08 09:11:42 +0530

    created a flutter project

---
+++ android/app/src/main/java/com/decodereign/scan_it/MainActivity.java
@@ -0,0 +1,6 @@
+package com.decodereign.scan_it;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/root.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
